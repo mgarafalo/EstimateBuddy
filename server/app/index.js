5f5453060682145e2055d637808e740a51b32663
Matthew Garafalo <matt9194@gmail.com>
2022-02-22 18:42:59 -0500

    Update index.js

--- server/app/index.js
+++ server/app/index.js
@@ -24,7 +24,7 @@ db.mongoose
     })
 
 app.use(cors())
-app.use(express.static(path.join(__dirname, 'build')))
+app.use(express.static(path.join(__dirname, './build')))
 
 // app.get('/*', (req, res) => {
 //     res.sendFile(path.join(__dirname, 'build', 'index.html'));
