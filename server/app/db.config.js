04c168fbe8005ad3356afe5c97fb41b51b7397f5
Matthew Garafalo <matt9194@gmail.com>
2022-02-23 13:08:07 -0500

    hiding mongo key

--- server/app/db.config.js
+++ server/app/db.config.js
@@ -2,6 +2,5 @@ const mongo = require("../config/mongo");
 
 module.exports = {
   // url: "mongodb://localhost:27017/EstimateBuddyTestV1"
-  // url: "mongodb+srv://admin:Mg91947$@cluster0.6bigj.mongodb.net/EstimateBuddy"
-  url: process.env.MONGO_URI || mongo.url
+  url: process.env.MONGO_URL || mongo.url
 };
--- server/app/models/index.js
+++ server/app/models/index.js
@@ -7,6 +7,7 @@ const db = {}
 
 db.mongoose = mongoose
 db.url = dbConfig.url
+console.log(db.url)
 db.estimates = require('./estimateRequest.models.js')(mongoose)
 db.shops = require('./shop.model')(mongoose)
 
