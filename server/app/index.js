17cd06adc5d6c9dbb561e41fd183add688206e45
Matthew Garafalo <matt9194@gmail.com>
2022-02-23 09:02:12 -0500

    Update index.js

--- server/app/index.js
+++ server/app/index.js
@@ -24,11 +24,11 @@ db.mongoose
     })
 
 app.use(cors())
-app.use(express.static(path.join(__dirname, 'build')))
+app.use(express.static(path.resolve(__dirname, './build')))
 
-app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+// app.get('/*', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
 
 app.get('/api/login', async (req, res) => {
     const shop = await db.shops.find({ username: req.query.username })
