382769236a702a6da45044c813d0f1c3ccb3d4da
Matthew Garafalo <matt9194@gmail.com>
2022-02-23 13:17:31 -0500

    db secrets!!!!!!!!

--- server/app/db.config.js
+++ server/app/db.config.js
@@ -1,6 +1,3 @@
-// const mongo = require("../config/mongo");
-
 module.exports = {
-  // url: "mongodb://localhost:27017/EstimateBuddyTestV1"
   url: process.env.MONGO_URL
 };
--- server/app/models/index.js
+++ server/app/models/index.js
@@ -1,13 +1,13 @@
 const dbConfig = require('../db.config')
 const mongoose = require('mongoose')
+const mongo = require('../../config/mongo')
 
 mongoose.Promise= global.Promise
 
 const db = {}
 
 db.mongoose = mongoose
-db.url = dbConfig.url
-console.log(db.url)
+db.url = dbConfig.url || mongo.url
 db.estimates = require('./estimateRequest.models.js')(mongoose)
 db.shops = require('./shop.model')(mongoose)
 
