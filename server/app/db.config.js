b28e17e7958c9ab5d88b0ced760427fc2bd988f6
Matthew Garafalo <matt9194@gmail.com>
2022-02-23 12:40:40 -0500

    Update db.config.js

--- server/app/db.config.js
+++ server/app/db.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   // url: "mongodb://localhost:27017/EstimateBuddyTestV1"
   // url: "mongodb+srv://admin:Mg91947$@cluster0.6bigj.mongodb.net/EstimateBuddy"
-  url: process.env.mongoConnString || "mongodb+srv://admin:Mg91947$@cluster0.6bigj.mongodb.net/EstimateBuddy"
+  url: "mongodb+srv://admin:Mg91947$@cluster0.6bigj.mongodb.net/EstimateBuddy"
 };
