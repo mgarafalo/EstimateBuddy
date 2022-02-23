1897240c2610fc47129c91cc18ca6caacdbfb290
Matthew Garafalo <matt9194@gmail.com>
2022-02-23 09:27:25 -0500

    heroku update

--- server/app/index.js
+++ server/app/index.js
@@ -24,10 +24,10 @@ db.mongoose
     })
 
 app.use(cors())
-app.use(express.static(path.resolve(__dirname, '../clientapp/build')))
+app.use(express.static(path.join(__dirname, 'build')))
 
 app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../clientapp/build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 app.get('/api/login', async (req, res) => {
