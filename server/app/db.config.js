a92adf411d687762a937a459d834e347901fd8ba
Matthew Garafalo <matt9194@gmail.com>
2022-02-23 12:51:06 -0500

    db secrets

--- server/app/db.config.js
+++ server/app/db.config.js
@@ -1,5 +1,7 @@
+const mongo = require("../config/mongo");
+
 module.exports = {
   // url: "mongodb://localhost:27017/EstimateBuddyTestV1"
   // url: "mongodb+srv://admin:Mg91947$@cluster0.6bigj.mongodb.net/EstimateBuddy"
-  url: "mongodb+srv://admin:Mg91947$@cluster0.6bigj.mongodb.net/EstimateBuddy"
+  url: process.env.MONGO_URI || mongo.url
 };
